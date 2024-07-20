9115c38e37696ed345ce3a6a0f9ba4719cffd763
David Manouchehri <david.manouchehri@ai.moda>
2024-07-20 13:53:02 +0000

    Add fetch handler to avoid seeing wrangler error spam.

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,8 @@ export interface Env {
 export default {
   async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext) {
     console.debug("TODO");
+  },
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return new Response(null, { status: 404 });
   }
 };
